48c8c7b4a27f700815ba1cab50a81db1b7b07a67
Ivan Zhukov <ziv@servers.com>
2023-11-02 12:09:30 +0200

    Added build method and tests

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,5 +4,36 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Tag
+    result = ''
+    def self.build(html_tag, *html_options)
+      params = []
+
+      html_options.each do |p|
+        p.each do |key, value|
+          params << "#{key}=\"#{value}\""
+        end
+      end
+
+      if params.any?
+        params = " " + params.join(" ")
+      else
+        params = ''
+      end
+
+      if html_tag == 'br'
+        result = '<br>'
+      elsif html_tag == 'img'
+        result = "<img#{params}>"
+      elsif html_tag == 'input'
+        result = "<input#{params}>"
+      elsif html_tag == 'label'
+        result = "<label#{params}>#{yield}</label>"
+      elsif html_tag == 'div'
+        result = '<div></div>'
+      end
+    end
+    result
+  end
 end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -1,13 +1,36 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require_relative "test_helper"
+require_relative "../lib/hexlet_code.rb"
 
 class TestHexletCode < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_build_method
+    expected = '<br>'
+    actual = HexletCode::Tag.build('br')
+    assert_equal expected, actual, "Expected #{expected}, but got #{actual}"
+
+    expected = '<img src="path/to/image">'
+    actual = HexletCode::Tag.build('img', src: 'path/to/image')
+    assert_equal expected, actual, "Expected #{expected}, but got #{actual}"
+
+    expected = '<input type="submit" value="Save">'
+    actual = HexletCode::Tag.build('input', type: 'submit', value: 'Save')
+    assert_equal expected, actual, "Expected #{expected}, but got #{actual}"
+
+    expected = '<label>Email</label>'
+    actual = HexletCode::Tag.build('label') { 'Email' }
+    assert_equal expected, actual, "Expected #{expected}, but got #{actual}"
+
+    expected = '<label for="email">Email</label>'
+    actual = HexletCode::Tag.build('label', for: 'email') { 'Email' }
+    assert_equal expected, actual, "Expected #{expected}, but got #{actual}"
+
+    expected = '<div></div>'
+    actual = HexletCode::Tag.build('div')
+    assert_equal expected, actual, "Expected #{expected}, but got #{actual}"
   end
 end
