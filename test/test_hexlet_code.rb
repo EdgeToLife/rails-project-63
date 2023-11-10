8d9369131f464e1860d16540de787571013e3143
Ivan Zhukov <ziv@servers.com>
2023-11-10 14:52:52 +0200

    create fixture for test

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -128,8 +128,7 @@ class TestHexletCode < Minitest::Test
 
   def test_form_for_with_submit
     user = User.new name: 'rob'
-    expected = "<form action='/profile' method='get' class='hexlet-form'>\
-<input type='submit' value='Save'></form>"
+    expected = File.read('./test/html-fixtures/test_form_for_with_submit.html')
     actual = HexletCode.form_for(user, url: '/profile', method: :get, class: 'hexlet-form', &(->(f) { f.submit }))
     assert_equal(expected.to_s, actual.to_s)
   end
