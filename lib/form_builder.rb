718c054e6aa047ef51d445a87957c1dbc81f31ba
Ivan Zhukov <ziv@servers.com>
2023-11-27 12:02:22 +0200

    refactor: optimeze node_add

--- lib/form_builder.rb
+++ lib/form_builder.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-# The HexletCode module
 module HexletCode
-  # Form Builder class
   class FormBuilder
     attr_reader :nodes
 
@@ -13,25 +11,16 @@ module HexletCode
 
     def input(name, options = {})
       @user.public_send(name)
-      label = Label.new(name)
-      add_node(label)
-      node = if options[:as] == :text
-               Text.new(@user, name, options)
-             else
-               Input.new(@user, name, options)
-             end
-      add_node(node)
+      @nodes << Label.new(name)
+      @nodes << if options[:as] == :text
+                  Text.new(@user, name, options)
+                else
+                  Input.new(@user, name, options)
+                end
     end
 
     def submit(name = 'Save')
-      node = Submit.new(name)
-      add_node(node)
-    end
-
-    private
-
-    def add_node(node)
-      @nodes << node
+      @nodes << Submit.new(name)
     end
   end
 end
