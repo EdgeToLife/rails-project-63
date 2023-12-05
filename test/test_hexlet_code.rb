556f2b64a243db78468d2920d4825b966f574939
Ivan Zhukov <ziv@servers.com>
2023-12-05 17:20:05 +0200

    refactor: removed build method tests

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -15,42 +15,6 @@ class TestHexletCode < Minitest::Test
     refute_nil ::HexletCode::VERSION
   end
 
-  def test_build_br
-    expected = '<br></br>'
-    actual = HexletCode::Tag.build('br')
-    assert_equal expected, actual, "Expected #{expected}, but got #{actual}"
-  end
-
-  def test_build_img
-    expected = "<img src='path/to/image'></img>"
-    actual = HexletCode::Tag.build('img', src: 'path/to/image')
-    assert_equal expected, actual, "Expected #{expected}, but got #{actual}"
-  end
-
-  def test_build_imput
-    expected = "<input type='submit' value='Save'></input>"
-    actual = HexletCode::Tag.build('input', type: 'submit', value: 'Save')
-    assert_equal expected, actual, "Expected #{expected}, but got #{actual}"
-  end
-
-  def test_build_label
-    expected = '<label>Email</label>'
-    actual = HexletCode::Tag.build('label') { 'Email' }
-    assert_equal expected, actual, "Expected #{expected}, but got #{actual}"
-  end
-
-  def test_build_label_wit_attrs
-    expected = "<label for='email'>Email</label>"
-    actual = HexletCode::Tag.build('label', for: 'email') { 'Email' }
-    assert_equal expected, actual, "Expected #{expected}, but got #{actual}"
-  end
-
-  def test_build_div
-    expected = '<div></div>'
-    actual = HexletCode::Tag.build('div')
-    assert_equal expected, actual, "Expected #{expected}, but got #{actual}"
-  end
-
   def test_form_for
     expected = "<form action='#' method='post'></form>"
     # rubocop:disable Lint/EmptyBlock
